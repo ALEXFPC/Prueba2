14c22327d180241f5fcd41d9b3bcff2d0092d2a7
ALEXFPC <40238910+ALEXFPC@users.noreply.github.com>
2018-06-13 10:45:40 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <div class="container">
     <footer>
-        <p>Incapaz ingeniería & Construcciones Ltda.</p>
-        <p>Dirección: Villa Jurel n°21, PuertoAysen, Chile.</p>
-        <P>Telefono Contacto: 2212309846</P>
-        <p>Informacion de contacto: <a href="correo.falso@gmail.com">contructora.Incapaz@gmail.com</a></p>
+        <p>Hospital Pelotillehue.</p>
+        <p>Dirección:Las Parcelas n°22, Santiago, Chile.</p>
+        <P>Telefono Contacto: 2273465738</P>
+        <p>Informacion de contacto: <a href="correo.falso@gmail.com">hspital_pelotillehue@gmail.com</a></p>
         <p>Todos Derechos reservado &copy; 2018</p>
     </footer>
 </div>
